0163dbcb1a3563cd4393a5a19b101551dcc666c1
Talon Bragg <pvpgamecrash@gmail.com>
2017-02-16 15:08:29 -0800

    Add things

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
     var gulp = require('gulp');
     var minifyCSS = require('gulp-minify-css');
     var pkg = require('./package.json');
-    
+    var comment = '\/*\r\n* Karat ' + pkg.version + '\r\n* Copyright 2017, Sid Hegde\r\n* http:\/\/website.com\/\r\n* Free to use under the MIT license.\r\n* https:\/\/github.com\/Sidthecoder\/Karat\/LICENSE\r\n*\/\r\n';
     
     gulp.task('minify', function() {
         return gulp.src(['src/grid.css', 'src/padding.css'])
