3208c44041bf33c9a6824d25ed05b2adfcfe2762
Shreyas Lad <shadowtemplates@gmail.com>
2017-02-17 16:58:49 -0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
     var gulp = require('gulp');
     var minifyCSS = require('gulp-minify-css');
     var pkg = require('./package.json');
-    var comment = '\/*\r\n* Karat ' + pkg.version + '\r\n* Copyright 2017, Sid Hegde\r\n* http:\/\/website.com\/\r\n* Free to use under the MIT license.\r\n* https:\/\/github.com\/Sidthecoder\/Karat\/LICENSE\r\n*\/\r\n';
+    var comment = '\/*\r\n* Karat ' + pkg.version + '\r\n* Copyright 2017, Sid Hegde\r\n* http:\/\/karat.gq\/\r\n* Free to use under the MIT license.\r\n* https:\/\/github.com\/Sidthecoder\/Karat\/LICENSE\r\n*\/\r\n';
     var $ = require('gulp-load-plugins')();    
 
     gulp.task('minify', function() {
