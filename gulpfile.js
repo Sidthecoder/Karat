552e93b3d0773b80e0dac31ea4a3f5efc023d79f
Talon Bragg <pvpgamecrash@gmail.com>
2017-02-18 19:29:38 -0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,5 @@
     var gulp = require('gulp');
-    var minifyCSS = require('gulp-minify-css');
+    var cleanCSS = require('gulp-clean-css');
     var pkg = require('./package.json');
     var comment = '\/*\r\n* Karat ' + pkg.version + '\r\n* Copyright 2017, Sid Hegde\r\n* http:\/\/karat.gq\/\r\n* Free to use under the MIT license.\r\n* https:\/\/github.com\/Sidthecoder\/Karat\/LICENSE\r\n*\/\r\n';
     var $ = require('gulp-load-plugins')();    
-
-    gulp.task('minify', function() {
-        return gulp.src(['public/grid.css', 'public/padding.css'])
-            .pipe(minifyCSS())
-            .pipe($.header(comment))
-            .pipe($.size())
-            .pipe($.concat('main.min.css'))
-            .pipe(gulp.dest('dist'));
-    });
-    
-    gulp.task('watch', function() {
-      gulp.watch(['src/*.css'], ['default']);
-    });
-
-gulp.task('default', ['minify']);
